3bf8748e88602f7f1f6849fb43b0c3cd612e5032
Kartik Singh Chuphal <64568294+kartik077-cr7@users.noreply.github.com>
2022-12-11 16:52:41 +0530

    minor-change

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const app = require('./app')
 
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
 	console.log('Server is running on port '+port);
